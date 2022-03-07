f9a043e3e15f7e51a40198f4ed8ddeb0ec9ea2d0
CesarChiodi <cesarchiodi@outlook.com>
2022-03-07 18:19:11 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace numero_primo_contador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, i, cont = 0;
+            Console.WriteLine("escolha um numero");
+            numero = int.Parse(Console.ReadLine());
+            for(i = 1; i <= numero; i++)
+            {
+                if(numero % i == 0)
+                {
+                    cont++;
+                }               
+            }
+            if (cont == 2)
+            {
+                Console.WriteLine("o numero {0} é primo", numero);
+            }
+            else
+            {
+                Console.WriteLine("o numero {0} nao é primo", numero);
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
+    
